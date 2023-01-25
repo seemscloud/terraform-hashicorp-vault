48c6624f7d4221983a97bfab73f96cd39e5129d0
Mateusz Katana <mateusz.katana@lppsa.com>
2023-01-26 00:54:08 +0100

    asdf

--- auth-kubernetes-binding.tf
+++ auth-kubernetes-binding.tf
@@ -5,16 +5,16 @@
 resource "vault_kubernetes_auth_backend_role" "example" {
   backend = vault_auth_backend.kubernetes.path
 
-  role_name = "ns-vault_system-sa-debug"
-
-  bound_service_account_names      = ["debug"]
-  bound_service_account_namespaces = ["default"]
+  role_name                        = "ns-vault_system-sa-nginx"
+  bound_service_account_names      = ["nginx", "vault"]
+  bound_service_account_namespaces = ["vault-system"]
 
   token_policies = [
     "capabilities-self",
-    "kv-foo"
+    "kv-foo",
+    "default"
   ]
-
+  
   depends_on = [
     vault_auth_backend.kubernetes
   ]
--- auth-kubernetes.tf
+++ auth-kubernetes.tf
@@ -7,6 +7,9 @@ resource "vault_auth_backend" "kubernetes" {
 }
 
 resource "vault_kubernetes_auth_backend_config" "example" {
-  backend                = vault_auth_backend.kubernetes.path
-  kubernetes_host        = "http://kubernetes.default.svc:443"
+  backend         = vault_auth_backend.kubernetes.path
+  kubernetes_host = "https://kubernetes.default.svc:443"
+
+  issuer                 = "api"
+  disable_iss_validation = "true"
 }
